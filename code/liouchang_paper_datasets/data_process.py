1059b46e33395b1157321bed448f0990609fec99
王天赐 <15565946702@163.com>
2021-10-27 15:28:02 +0800

    修改term的位from和to的下标位置

--- code/liouchang_paper_datasets/data_process.py
+++ code/liouchang_paper_datasets/data_process.py
@@ -12,6 +12,23 @@ def read_json_data(data_path):
     return laptop_data
 
 
+def ensure_index(text_line, from_idx, to_idx, term):
+    """
+        注意细节 : 区间是 [from_, to) 的
+    :param text_line:
+    :param from_idx:
+    :param to_idx:
+    :param term:
+    :return:
+    """
+    if text_line[from_idx:to_idx] != term:
+        from_ = text_line.find(term)
+        to_ = from_ + len(term) - 1
+        return from_, to_
+    else:
+        return from_idx, to_idx
+
+
 def json_to_xml(df):
     columns = df.columns
     # 设置根标签
@@ -26,16 +43,25 @@ def json_to_xml(df):
         # 设置 text 标签内容
         text.text = df[column]['sentence']
 
+        text_line = df[column]['sentence']
+        from_idx = df[column]['from']
+        to_idx = df[column]['to']
+        term = df[column]['term']
+
+        # 验证 from 和 to 是否正确
+        # 如果不正确获取新的from和to的下标
+        from_idx, to_idx = ensure_index(text_line, from_idx, to_idx, term)
+
         # 添加 aspectTerms 标签
         aspectTerms = ET.SubElement(sentence, 'aspectTerms')
         # 添加 aspectTerms 标签, aspectTerm 表亲的父标签为 aspectTerms
         aspectTerm = ET.SubElement(aspectTerms, 'aspectTerm')
         # 设置 aspectTerm标签 属性
         aspectTerm.attrib = {
-            'from': str(df[column]['from']),
-            'to': str(df[column]['to']),
+            'from': str(from_idx),
+            'to': str(to_idx),
             'polarity': str(df[column]['polarity']),
-            'term': str(df[column]['term'])
+            'term': str(term)
         }
 
     xml_data = ET.tostring(root)
@@ -61,10 +87,10 @@ if __name__ == '__main__':
     files = ['dev', 'test', 'train']
 
     for file in tqdm(files):
-        laptop_input = os.path.join(base_in_path, "laptop", file+".json")
-        rest_input = os.path.join(base_in_path, "rest", file+".json")
-        laptop_output = os.path.join(base_out_path, "laptop", file+".xml")
-        rest_output = os.path.join(base_out_path, "rest", file+".xml")
+        laptop_input = os.path.join(base_in_path, "laptop", file + ".json")
+        rest_input = os.path.join(base_in_path, "rest", file + ".json")
+        laptop_output = os.path.join(base_out_path, "laptop", file + ".xml")
+        rest_output = os.path.join(base_out_path, "rest", file + ".xml")
 
         run_func(laptop_input, laptop_output)
         run_func(rest_input, rest_output)
